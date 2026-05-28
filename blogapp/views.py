f68d9c167bd75f96ba02b4e21cd15e3d81fdf3b4
harshvardhan1907-19 <sagarharshvardhan9@gmail.com>
2026-05-28 15:23:26 +0530

    Major UI/UX changes

--- blogapp/views.py
+++ blogapp/views.py
@@ -149,7 +149,7 @@ def like_post(request, pk):
         'total_likes': post.likes.count()
     })
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content', 'category', 'image']
     template_name = "blog/post_form.html"
