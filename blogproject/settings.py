10a5fdfbbc538cacc767f8bb3137973e9a8cfe86
harshvardhan1907-19 <sagarharshvardhan9@gmail.com>
2026-06-03 09:42:52 +0530

    Add email verification change

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -209,7 +209,8 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
     # When you deploy to Render:
 
